8875a2c46236d9f18dee2367f29d9d876bf4f7c9
sg05c <141105618+sg05c@users.noreply.github.com>
2023-11-28 09:44:49 +0530

    fetched 2nd api

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
 fetch('https://www.themealdb.com/api/json/v1/1/random.php')
 .then(response => response.json())
-.then((parsedData) => {
-  console.log(parsedData)
+.then((parsedDataRandom) => {
+  console.log(parsedDataRandom)
+})
+
+fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+.then(response => response.json())
+.then((parsedDataSearch) => {
+  console.log(parsedDataSearch)
 })
