9cef0ceb6fffe86c90faea87d2fbb57de5204e5e
sg05c <141105618+sg05c@users.noreply.github.com>
2023-11-28 21:17:01 +0530

    final commit

--- scripts.js
+++ scripts.js
@@ -44,6 +44,7 @@ fetch(apiUrl)
 .then(parsedData => {
 const meals = parsedData.meals;
 gridContainer.innerHTML = '';
+// console.log(parsedData.meals)
 
 meals.forEach(meal => {
     const mealDiv = document.createElement('div');
@@ -65,3 +66,65 @@ meals.forEach(meal => {
 });
 });
 
+
+// const button = document.getElementsByClassName('buttonscroll');
+// button.addEventListener('click', () => {
+//   window.scrollTo({
+//     top: document.documentElement.scrollHeight,
+//     behavior: 'smooth'
+//   });
+// });
+
+
+
+function displayRandomData(meals) {
+  const randomMeal = meals[Math.floor(Math.random() * meals.length)];
+
+  const image = document.createElement('img');
+  image.src = randomMeal.strMealThumb;
+
+  const title = document.createElement('h1');
+  title.textContent = randomMeal.strMeal;
+
+  const randomImgs = document.querySelector('.randomImgs');
+  randomImgs.appendChild(image);
+  randomImgs.appendChild(title);
+
+  randomImgs.addEventListener('click', function() {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${randomMeal.idMeal}`)
+      .then(response => response.json())
+      .then(data => {
+        const ingredients = [];
+        for (let i = 1; i <= 20; i++) {
+          if (data.meals[0][`strIngredient${i}`]) {
+            const ingredient = data.meals[0][`strIngredient${i}`];
+            const measure = data.meals[0][`strMeasure${i}`];
+            ingredients.push(`${ingredient} - ${measure}`);
+          }
+        }
+
+        // Create a modal to display the ingredients
+        const modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            <h2>Ingredients</h2>
+            <ul>
+              ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+            </ul>
+          </div>
+        `;
+        document.body.appendChild(modal);
+
+        // Close the modal when the close button is clicked
+        const closeButton = modal.querySelector('.close');
+        closeButton.addEventListener('click', function() {
+          modal.remove();
+        });
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  });
+}
